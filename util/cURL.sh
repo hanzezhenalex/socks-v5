5ed5af7ce155f54fa160d6d6ffe954fd21600270
hanzezhenalex <hanalex109@gmail.com>
2023-05-12 16:29:10 +0800

    add script for usrname/passwd socks proxy

--- util/cURL.sh
+++ util/cURL.sh
@@ -7,3 +7,6 @@ set -x
 
 # In curl >= 7.21.7, use
 curl http://www.baidu.com -x socks5h://localhost:8010
+
+# for username/password, use
+# curl http://www.baidu.com -x socks5h://username:password@localhost:8010
